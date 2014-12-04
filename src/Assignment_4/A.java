5640e490d74749e96ccf2dee336ac6f8dc9729ab
koonchaim <koonchaim@gmail.com>
2014-12-04 16:11:09 +0700

    Success

---
+++ src/Assignment_4/A.java
@@ -0,0 +1,43 @@
+package Assignment_4;
+
+public class A {
+	private String a1;
+	private int a2;
+	private int a3;
+
+	public A() {
+		super();
+	}
+
+	public A(String a1, int a2, int a3) {
+		super();
+		this.a1 = a1;
+		this.a2 = a2;
+		this.a3 = a3;
+	}
+
+	public String getA1() {
+		return a1;
+	}
+
+	public void setA1(String a1) {
+		this.a1 = a1;
+	}
+
+	public int getA2() {
+		return a2;
+	}
+
+	public void setA2(int a2) {
+		this.a2 = a2;
+	}
+
+	public int getA3() {
+		return a3;
+	}
+
+	public void setA3(int a3) {
+		this.a3 = a3;
+	}
+
+}
--- src/Assignment_4/pqr.java
+++ src/Assignment_4/pqr.java
@@ -2,36 +2,72 @@ package Assignment_4;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class pqr {
-	static class PQsort implements Comparator<Integer> {
-		public int compare(Integer one, Integer two) {
-			return two - one;
+	static class PQsort implements Comparator<A> {
+		public int compare(A one, A two) {
+			return two.getA2() - one.getA2();
 		}
 	}
 
 	public static void main(String[] args) {
-
-		int[] ia = { 1, 11, 5, 3, 4, 7, 6, 9, 8 };
 		PQsort pqs = new PQsort();
-		PriorityQueue<Integer> pq2 = new PriorityQueue<Integer>(pqs);
-		for (int x : ia) {
-			pq2.offer(x);
-		}
-		// System.out.println("pq2: " + pq2);
-		// System.out.println("peek: " + pq2.peek());
-		// System.out.println("poll: " + pq2.poll());
-		// System.out.println("size: " + pq2.size());
-		// System.out.println("pq2: " + pq2);
+		PriorityQueue<A> pq = new PriorityQueue<A>(pqs);
+		pq.add(new A("โกคู 1", 8, 4));
+		pq.add(new A("พิคโคโร", 5, 4));
 
-		for (int i = 0; i < ia.length; i++) {
-			System.out.println("pq2: " + pq2);
-			System.out.println("peek: " + pq2.peek());
-			System.out.println("poll: " + pq2.poll());
-			System.out.println("size: " + pq2.size());
-			System.out.println("pq2: " + pq2);
-			System.out.println("====================");
-		}
+		Queue<A> customerPriorityQueue = pq;
 
+		PQsort pqs1 = new PQsort();
+		PriorityQueue<A> pq1 = new PriorityQueue<A>(pqs1);
+		pq1.add(new A("ศัตรู 1", 5, 3));
+		pq1.add(new A("ศัตรู 2", 8, 4));
+		pq1.add(new A("ศัตรู 3", 1, 5));
+		pq1.add(new A("ศัตรู 4", 3, 2));
+		Queue<A> customerPriorityQueue1 = pq1;
+
+		pollDataFromPriorityQueue(customerPriorityQueue, customerPriorityQueue1);
+	}
+
+	// utility method to poll data from queue
+	private static void pollDataFromQueue(Queue<A> customerPriorityQueue) {
+		while (true) {
+			A cust = customerPriorityQueue.poll();
+			if (cust == null)
+				break;
+			System.out.println("name : " + cust.getA1() + "\t: " + cust.getA2() + "\t"
+					+ cust.getA3());
+		}
+	}
+
+	// ================================================================================
+	private static void pollDataFromPriorityQueue(Queue<A> queue1, Queue<A> queue2) {
+		while (true) {
+			A c1 = queue1.peek();
+			A c2 = queue2.peek();
+
+			if (c1 == null && c2 == null) {
+				break;
+			} else if (c1 == null && c2 != null) {
+				System.out.println("- " + c2.getA1() + "\tvs " + "โกคู หรือ พิคโคโร");
+				queue2.poll();
+			} else if (c1.getA2() == c2.getA2() && c1.getA3() > c2.getA3()) {
+				System.out.println("- " + c1.getA1() + "\tvs " + c2.getA1());
+				queue1.poll();
+			} else if (c2.getA2() > c1.getA2()) {
+				System.out.println("- " + c2.getA1() + "\tvs " + c1.getA1());
+				queue2.poll();
+			} else if (c1.getA2() == c2.getA2() && c1.getA3() == c2.getA3()) {
+				System.out.println("- " + c1.getA1() + "\tvs " + c2.getA1());
+				queue1.poll();
+			} else if (c1.getA2() > c2.getA2()) {
+				System.out.println("- " + c1.getA1() + "\tvs " + c2.getA1());
+				queue1.poll();
+			} else if (c2.getA2() == c1.getA2() && c2.getA3() > c1.getA3()) {
+				System.out.println("- " + c2.getA1() + "\tvs " + c1.getA1());
+				queue2.poll();
+			}
+		}
 	}
 }
